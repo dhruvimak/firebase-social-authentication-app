aa95390c17b6491a9f851a02497028c877c271be
dhruvimak <dhruvipmakavana@yahoo.com>
2018-09-06 16:42:08 +0530

    heroku

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const express = require('express');
-const http = require('http');
+const compression = require('compression');
 const path = require('path');
-
-const app=express();
-
-
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 4200;
+
+// Gzip
+app.use(compression());
+
+// Run the app by serving the static files in the dist directory
 app.use(express.static(__dirname + '/dist'));
-app.listen(port, () => console.log('Example app listening on port 4200!'))
 
-app.get('/*', (req, res) => res.sendFile(path.join(_dirname+'/dist/index.html')));
+// Start the app by listening on the default Heroku port
+app.listen(port);
 
+// For all GET requests, send back index.html so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+console.log(`Server listening on ${port}`);
