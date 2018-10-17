cc8c2340126ffae99eee61f8e680d9f545f738b6
dhruvimak <dhruvipmakavana@yahoo.com>
2018-10-17 14:47:58 +0530

    final

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const port = process.env.PORT || 4200;
 app.use(compression());
 
 // Run the app by serving the static files in the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/firebase-social-auth/'));
 
 // Start the app by listening on the default Heroku port
 app.listen(port);
 
 // For all GET requests, send back index.html so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
 
 console.log(`Server listening on ${port}`);
